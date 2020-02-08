8b21e12229b5ff05df220b21a098a7bdf6ceccfd
ZerOx4C <zerox4c@gmail.com>
2020-02-08 18:54:38 +0900

    separate logic from view

--- arknights/script.js
+++ arknights/script.js
@@ -30,238 +30,322 @@ PATTERN_MASTER_SOURCE.forEach(function(row, index) {
     }
 });
 
-var selectedFlags = 0;
-var tagFilter = null;
+var tagStatusTable = {};
+var patternStatusTable = {};
+var unitStatusTable = {};
 
-function initializeTagArea() {
-    var tagAreaElement = document.getElementById("tagArea");
-    Array.from(tagAreaElement.children).forEach(function (tagElement) {
-        tagElement.remove();
+var tagElementList = [];
+var unitElementList = [];
+var unitPatternElementList = [];
+var unitTagElementList = [];
+
+// NOTE: workaround.
+var unitFilterElementList = [];
+
+var selectedFlags = 0;
+var tagFilter = "";
+
+function getTagIdListByFlags(flags) {
+    var ret = [];
+
+    Object.keys(TagMaster).forEach(function (tagId) {
+        var tag = TagMaster[tagId];
+
+        if (tag.flag & flags) {
+            ret.push(tagId);
+        }
     });
 
-    var tagTemplateElement = document.getElementById("tagTemplate");
-    var tagElementOrigin = tagTemplateElement.content.querySelector("a");
+    return ret;
+}
 
-    Object.keys(TagMaster).forEach(function(tagId) {
-        var tag = TagMaster[tagId];
-        var tagElement = document.importNode(tagElementOrigin, true);
-        tagElement.dataset.flag = tag.flag;
-        tagElement.dataset.id = tagId;
-        tagElement.text = tag.name;
-        tagAreaElement.appendChild(tagElement);
+function initializeTagStatus() {
+    tagStatusTable = {};
+
+    Object.keys(TagMaster).forEach(function (tagId) {
+        tagStatusTable[tagId] = {
+            flagMatched: false,
+            filterMatched: false,
+        };
     });
 }
 
-function updateTagArea() {
-    var tagAreaElement = document.getElementById("tagArea");
-    if (tagFilter) {
-        tagAreaElement.classList.add("filtered");
-    }
-    else {
-        tagAreaElement.classList.remove("filtered");
-    }
+function initializePatetrnStatus() {
+    patternStatusTable = {};
 
-    Array.from(tagAreaElement.children).forEach(function(tagElement) {
-        if (selectedFlags & tagElement.dataset.flag) {
-            tagElement.classList.add("selected");
+    Object.keys(PatternMaster).forEach(function (patternId) {
+        var pattern = PatternMaster[patternId];
+
+        patternStatusTable[patternId] = {
+            flagMatched: false,
+            essentialTagIdList: getTagIdListByFlags(pattern.essentialFlags),
+            optionalTagIdList: getTagIdListByFlags(pattern.optionalFlags),
+        };
+    });
+}
+
+function initializeUnitStatus() {
+    unitStatusTable = {};
+
+    var unitIdToPatternStatusListTable = {};
+    Object.keys(PatternMaster).forEach(function (patternId) {
+        var pattern = PatternMaster[patternId];
+        var patternIdList = unitIdToPatternStatusListTable[pattern.unitId] || [];
+        patternIdList.push(patternId);
+        unitIdToPatternStatusListTable[pattern.unitId] = patternIdList;
+    });
+
+    Object.keys(UnitMaster).forEach(function (unitId) {
+        unitStatusTable[unitId] = {
+            flagMatched: false,
+            patternIdList: unitIdToPatternStatusListTable[unitId] || [],
+        };
+    });
+}
+
+function updateTagStatus() {
+    Object.keys(tagStatusTable).forEach(function (tagId) {
+        var tagStatus = tagStatusTable[tagId];
+        var tag = TagMaster[tagId];
+
+        if (tagFilter == "") {
+            tagStatus.filterMatched = true;
+        }
+        else if (0 <= tag.ruby.indexOf(tagFilter)) {
+            tagStatus.filterMatched = true;
+        }
+        else if (0 <= tag.lowerName.indexOf(tagFilter)) {
+            tagStatus.filterMatched = true;
         }
         else {
-            tagElement.classList.remove("selected");
+            tagStatus.filterMatched = false;
         }
 
-        if (!tagFilter) {
-            tagElement.classList.remove("matched");
+        if (tag.flag & selectedFlags) {
+            tagStatus.flagMatched = true;
+        }
+        else {
+            tagStatus.flagMatched = false;
+        }
+    });
+}
+
+function updatePatetrnStatus() {
+    Object.keys(patternStatusTable).forEach(function (patternId) {
+        var patternStatus = patternStatusTable[patternId];
+
+        patternStatus.flagMatched = patternStatus.essentialTagIdList.some(function (tagId) {
+            var tagStatus = tagStatusTable[tagId];
+            return tagStatus.flagMatched;
+        });
+
+        if (patternStatus.flagMatched || 0 < patternStatus.essentialTagIdList.length) {
             return;
         }
 
-        var tag = TagMaster[tagElement.dataset.id];
-        if (0 <= tag.ruby.indexOf(tagFilter) || 0 <= tag.lowerName.indexOf(tagFilter)) {
-            tagElement.classList.add("matched");
-        }
-        else {
-            tagElement.classList.remove("matched");
-        }
+        patternStatus.flagMatched = patternStatus.optionalTagIdList.some(function (tagId) {
+            var tagStatus = tagStatusTable[tagId];
+            return tagStatus.flagMatched;
+        });
     });
 }
 
-function initializePatternArea() {
-    var resultAreaElement = document.getElementById("resultArea");
-    Array.from(resultAreaElement.children).forEach(function (patternElement) {
-        patternElement.remove();
-    });
+function updateUnitStatus() {
+    Object.keys(unitStatusTable).forEach(function (unitId) {
+        var unitStatus = unitStatusTable[unitId];
 
-    var resultTemplateElement = document.getElementById("resultTemplate");
-    var resultElementOrigin = resultTemplateElement.content.querySelector(".result");
-
-    var unitIdToPatternIdListTable = {};
-    Object.keys(PatternMaster).forEach(function (patternId) {
-        var pattern = PatternMaster[patternId];
-        var unit = UnitMaster[pattern.unitId];
-        var patternIdList = unitIdToPatternIdListTable[unit.id] || [];
-        patternIdList.push(patternId);
-        unitIdToPatternIdListTable[unit.id] = patternIdList;
-    });
-
-    Object.keys(unitIdToPatternIdListTable).forEach(function (unitId) {
-        var patternIdList = unitIdToPatternIdListTable[unitId];
-        var resultElement = document.importNode(resultElementOrigin, true);
-        fillResultElement(resultElement, unitId, patternIdList);
-        resultAreaElement.appendChild(resultElement);
+        unitStatus.flagMatched = unitStatus.patternIdList.some(function (patternId) {
+            var patternStatus = patternStatusTable[patternId]
+            return patternStatus.flagMatched;
+        });
     });
 }
 
-function fillResultElement(resultElement, unitId, patternIdList) {
+function initializeTagListView() {
+    tagElementList = [];
+
+    var tagListElement = document.getElementById("tagList");
+    var tagTemplateElement = document.getElementById("tagTemplate");
+    var tagElementOrigin = tagTemplateElement.content.querySelector("a");
+
+    // NOTE: workaround.
+    tagListElement.classList.add("filtered");
+
+    Array.from(tagListElement.children).forEach(function (tagElement) {
+        tagElement.remove();
+    });
+
+    Object.keys(tagStatusTable).forEach(function (tagId) {
+        var tag = TagMaster[tagId];
+        var tagElement = document.importNode(tagElementOrigin, true);
+        tagElement.dataset.tagId = tagId;
+        tagElement.text = tag.name;
+        tagElementList.push(tagElement);
+        tagListElement.appendChild(tagElement);
+    });
+}
+
+function initializeUnitListView() {
+    unitElementList = [];
+    unitPatternElementList = [];
+    unitTagElementList = [];
+
+    var unitListElement = document.getElementById("unitList");
+    var unitTemplateElement = document.getElementById("resultTemplate");
+    var unitElementOrigin = unitTemplateElement.content.querySelector(".result");
+
+    // NOTE: workaround.
+    unitFilterElementList.push(unitListElement);
+
+    Array.from(unitListElement.children).forEach(function (unitElement) {
+        unitElement.remove();
+    });
+
+    Object.keys(unitStatusTable).forEach(function (unitId) {
+        var unitElement = document.importNode(unitElementOrigin, true);
+        fillUnitElement(unitElement, unitId);
+        unitElementList.push(unitElement);
+        unitListElement.appendChild(unitElement);
+    });
+}
+
+function fillUnitElement(unitElement, unitId) {
+    var unitNameElement = unitElement.querySelector(".unit-name");
+    var unitNoteElement = unitElement.querySelector(".unit-note");
+    var patternListElement = unitElement.querySelector(".pattern-list");
+    var patternElementOrigin = unitElement.querySelector(".pattern");
+
+    // NOTE: workaround.
+    unitFilterElementList.push(patternListElement);
+
+    var unitStatus = unitStatusTable[unitId];
     var unit = UnitMaster[unitId];
-
-    var unitNameElement = resultElement.querySelector(".unit-name");
-    var unitNoteElement = resultElement.querySelector(".unit-note");
-    var patternListElement = resultElement.querySelector(".pattern-list");
-    var patternElementOrigin = resultElement.querySelector(".pattern");
-    patternElementOrigin.remove();
-
+    unitElement.dataset.unitId = unitId;
     unitNameElement.innerHTML = unit.name;
     unitNoteElement.innerHTML = unit.note;
-    resultElement.dataset.patternIdList = patternIdList;
 
-    patternIdList.forEach(function(patternId) {
+    patternElementOrigin.remove();
+
+    unitStatus.patternIdList.forEach(function (patternId) {
         var patternElement = document.importNode(patternElementOrigin, true);
-        fillResultPatternElement(patternElement, patternId);
+        fillUnitPatternElement(patternElement, patternId);
+        unitPatternElementList.push(patternElement);
         patternListElement.appendChild(patternElement);
     });
 }
 
-function fillResultPatternElement(patternElement, patternId) {
-    var pattern = PatternMaster[patternId];
-
+function fillUnitPatternElement(patternElement, patternId) {
     var essentialTagListElement = patternElement.querySelector(".tag-list.essential");
     var optionalTagListElement = patternElement.querySelector(".tag-list:not(.essential)");
     var essentialTagElementOrigin = essentialTagListElement.querySelector(".tag");
     var optionalTagElementOrigin = optionalTagListElement.querySelector(".tag");
     var noteElement = patternElement.querySelector(".note");
+
+    var patternStatus = patternStatusTable[patternId];
+    var pattern = PatternMaster[patternId];
+    patternElement.dataset.patternId = patternId;
+    noteElement.innerHTML = pattern.note;
+
     essentialTagElementOrigin.remove();
     optionalTagElementOrigin.remove();
 
-    patternElement.dataset.id = patternId;
-    noteElement.innerHTML = pattern.note;
+    patternStatus.essentialTagIdList.forEach(function (tagId) {
+        var tagElement = document.importNode(essentialTagElementOrigin, true);
+        fillUnitTagElement(tagElement, tagId);
+        unitTagElementList.push(tagElement);
+        essentialTagListElement.appendChild(tagElement);
+    });
 
-    Object.keys(TagMaster).forEach(function(tagId) {
-        var tag = TagMaster[tagId];
-
-        if (tag.flag & pattern.optionalFlags) {
-            var tagElement = document.importNode(optionalTagElementOrigin, true);
-            fillResultPatternTagElement(tagElement, tagId);
-            optionalTagListElement.appendChild(tagElement);
-        }
-        else if (tag.flag & pattern.essentialFlags) {
-            var tagElement = document.importNode(essentialTagElementOrigin, true);
-            fillResultPatternTagElement(tagElement, tagId);
-            essentialTagListElement.appendChild(tagElement);
-        }
+    patternStatus.optionalTagIdList.forEach(function (tagId) {
+        var tagElement = document.importNode(optionalTagElementOrigin, true);
+        fillUnitTagElement(tagElement, tagId);
+        unitTagElementList.push(tagElement);
+        optionalTagListElement.appendChild(tagElement);
     });
 }
 
-function fillResultPatternTagElement(tagElement, tagId) {
+function fillUnitTagElement(tagElement, tagId) {
     var tag = TagMaster[tagId]
+    tagElement.dataset.tagId = tagId;
     tagElement.innerHTML = tag.name;
-    tagElement.dataset.flag = tag.flag;
 }
 
-function updateResultArea() {
-    var resultAreaElement = document.getElementById("resultArea");
-    if (selectedFlags) {
-        resultAreaElement.classList.add("filtered");
-    }
-    else {
-        resultAreaElement.classList.remove("filtered");
-    }
-
-    Array.from(resultAreaElement.children).forEach(updateResultElement);
+function updateTagListView() {
+    tagElementList.forEach(function (tagElement) {
+        var tagStatus = tagStatusTable[tagElement.dataset.tagId];
+        tagElement.classList.toggle("selected", tagStatus.flagMatched);
+        tagElement.classList.toggle("matched", tagStatus.filterMatched);
+    });
 }
 
-function updateResultElement(resultElement) {
-    var anyPatternSatisfied = resultElement.dataset.patternIdList.split(",").some(function(patternId) {
-        var pattern = PatternMaster[patternId];
+function updateUnitListView() {
+    var filtered = 0 < selectedFlags;
 
-        if (pattern.essentialFlags <= 0 && pattern.optionalFlags & selectedFlags) {
-            return true;
-        }
-        else if (pattern.essentialFlags & selectedFlags) {
-            return true;
-        }
+    unitFilterElementList.forEach(function (element) {
+        element.classList.toggle("filtered", filtered);
     });
 
-    if (anyPatternSatisfied) {
-        resultElement.classList.add("matched");
-    }
-    else {
-        resultElement.classList.remove("matched");
-    }
+    unitElementList.forEach(function (unitElement) {
+        var unitStatus = unitStatusTable[unitElement.dataset.unitId];
+        unitElement.classList.toggle("matched", unitStatus.flagMatched);
+    });
 
-    var patternListElement = resultElement.querySelector(".pattern-list");
-    if (selectedFlags) {
-        patternListElement.classList.add("filtered");
-    }
-    else {
-        patternListElement.classList.remove("filtered");
-    }
+    unitPatternElementList.forEach(function (patternElement) {
+        var patternStatus = patternStatusTable[patternElement.dataset.patternId];
+        patternElement.classList.toggle("matched", patternStatus.flagMatched);
+    });
 
-    Array.from(patternListElement.children).forEach(updateResultPatternElement);
-}
+    unitTagElementList.forEach(function (tagElement) {
+        var tagStatus = tagStatusTable[tagElement.dataset.tagId];
+        tagElement.classList.toggle("satisfied", tagStatus.flagMatched);
 
-function updateResultPatternElement(patternElement) {
-    var pattern = PatternMaster[patternElement.dataset.id];
-
-    if (pattern.essentialFlags <= 0 && pattern.optionalFlags & selectedFlags) {
-        patternElement.classList.add("matched");
-    }
-    else if (pattern.essentialFlags & selectedFlags) {
-        patternElement.classList.add("matched");
-    }
-    else {
-        patternElement.classList.remove("matched");
-    }
-
-    var essentialTagListElement = patternElement.querySelector(".tag-list.essential");
-    Array.from(essentialTagListElement.children).forEach(updateResultPatternTagElement);
-
-    var optionalTagListElement = patternElement.querySelector(".tag-list:not(.essential)");
-    Array.from(optionalTagListElement.children).forEach(updateResultPatternTagElement);
-}
-
-function updateResultPatternTagElement(tagElement) {
-    if (tagElement.dataset.flag & selectedFlags) {
-        tagElement.classList.add("satisfied");
-    }
-    else {
-        tagElement.classList.remove("satisfied");
-    }
+        // NOTE: workaround.
+        tagElement.classList.add("matched");
+    });
 }
 
 function onTagFilterChanged(tagFilterElement) {
-    if (tagFilterElement.value != "") {
-        tagFilter = tagFilterElement.value.toLowerCase();
-    }
-    else {
-        tagFilter = null;
-    }
-    updateTagArea();
+    tagFilter = tagFilterElement.value.toLowerCase();
+
+    updateTagStatus();
+    updateTagListView();
 }
 
 function onTagClicked(tagElement) {
-    selectedFlags ^= tagElement.dataset.flag;
-    updateTagArea();
-    updateResultArea();
+    var tag = TagMaster[tagElement.dataset.tagId];
+    selectedFlags ^= tag.flag;
+
+    updateTagStatus();
+    updatePatetrnStatus();
+    updateUnitStatus();
+
+    updateTagListView();
+    updateUnitListView();
 }
 
 function onResetClicked() {
     selectedFlags = 0;
-    updateTagArea();
-    updateResultArea();
+
+    updateTagStatus();
+    updatePatetrnStatus();
+    updateUnitStatus();
+
+    updateTagListView();
+    updateUnitListView();
 }
 
 function onBodyLoaded() {
-    initializeTagArea();
-    initializePatternArea();
-    updateTagArea();
-    updateResultArea();
+    initializeTagStatus();
+    initializePatetrnStatus();
+    initializeUnitStatus();
+
+    updateTagStatus();
+    updatePatetrnStatus();
+    updateUnitStatus();
+
+    initializeTagListView();
+    initializeUnitListView();
+
+    updateTagListView();
+    updateUnitListView();
 }
